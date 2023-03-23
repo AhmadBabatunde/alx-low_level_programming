12c7c364b63825bb9ae058c7dc06cccc3e718109
my <ahmad@gmail.com>
2023-03-23 15:07:56 -0700

    go

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,20 +6,37 @@
  */
 void print_square(int size)
 {
- if (size <= 0)
- {
- _putchar('\n');
- } else
- {
- int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
- for (i = 0; i < size; i++)
- {
- for (j = 0; j < size; j++)
- {
- _putchar('#');
- }
- _putchar('\n');
- }
- }
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
+8-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
 }
