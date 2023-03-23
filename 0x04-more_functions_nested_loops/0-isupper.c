ff38ec7236a659e7e07e92abd0f48fc74df86a9d
my <ahmad@gmail.com>
2023-03-23 12:37:47 -0700

    go

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-/* Main - Entry point
+/*
+ *Main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int _isupper(int c)
 {
-	if (c >='A'&& c<='Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
