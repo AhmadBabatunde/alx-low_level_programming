1a91e8a6615e9d90ce8a20aeeed913290e742fca
my <ahmad@gmail.com>
2023-03-24 01:23:21 -0700

    go

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-/*
- *Main - Entry point
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Return: Always 0 (Success)
-*/
+ * Return: 0 or 1
+ */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
